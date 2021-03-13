d4d9a924768a2b7e4d4f535d18fd56db56a77f01
soniafriesen <58013701+soniafriesen@users.noreply.github.com>
2021-03-12 20:14:23 -0500

    Add files via upload

---
+++ Exercise 3/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*
+ * Program: User Class
+ * Purpose: A object class for the client
+ * Coder: Sonia Friesen, 0813682
+ * Date: Due Feburary 8th 2021
+ */
+namespace Exercise3_SF
+{
+    public class User
+    {
+        public string loginName;
+        public string password;
+        public string role;
+        public int transactions = 0; 
+
+        //default constructor
+        public User() { }
+
+        //three parameter constructor
+        public User(string login, string password, string role)
+        {
+            loginName = login;
+            this.password = password;
+            this.role = role;
+        }        
+    }
+}
