ca1c99388a59d9149efae46678fd868604d5da16
soniafriesen <58013701+soniafriesen@users.noreply.github.com>
2021-04-18 18:48:29 -0400

    Add files via upload

---
+++ Exercise5/ListExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+/*
+    Program: ListExtensions static class
+    Purpose: Single method to compare two lists of type T(anything)
+    Coder: Sonia Friesen, 0813682
+    Date: Due March 22nd, 2021
+ */
+namespace Exercise5_SF
+{
+    public static class ListExtensions
+    {
+        //Compares two lists, returns true or false
+        public static bool CompareTo<T>(this List<T> a, List<T> b)
+        {
+            //false return is list a has more or less elements than list b
+            if (a.Count != b.Count)
+                return false;
+            //returns true to both null
+            if (a.Count == 0 && b.Count == 0)
+                return true;            
+            else //they have the same length, lets check the values 
+            {
+                bool contains = false;
+                //checks if item in a is in list b
+                foreach (T item in a)
+                {
+                    if (b.Contains(item))
+                        contains = true; //if all the values are in both list the same, does not matter order
+                    else
+                        contains = false; //if one is not the same, its returns false
+                }
+                if (contains == false) //checking the contains bool variable
+                    return false;
+                else return true; //all good, return true
+            }            
+        }
+    }
+}
---
+++ Exercise5/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+    Program: Exercise5_SF
+    Purpose: Demonstrate Extension methods understanding
+    Coder: Sonia Friesen, 0813682
+    Date: Due March 22nd, 2021
+ */
+namespace Exercise5_SF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating (a) Same two string lists in a different order");
+            //list string A
+            List<string> stringA = new List<string>();
+            string a = "bear";
+            stringA.Add(a);
+            a = "fish";
+            stringA.Add(a);
+            a = "lizard";
+            stringA.Add(a);
+            //list stringB
+            List<string> stringB = new List<string>();
+            a = "lizard";
+            stringB.Add(a);
+            a = "bear";
+            stringB.Add(a);
+            a = "fish";
+            stringB.Add(a);
+            //testing CompareTo()
+            Console.WriteLine($"Results from CompareTo() on (a) is: {stringA.CompareTo(stringB)}");
+
+            //b Different two string lists with the same length
+            Console.WriteLine();
+            Console.WriteLine("Creating (b) Different two string lists with the same length");
+            //should return false, because it has the same # of elements  (which is required logically to compare different lists) but different contains different words.
+            List<string> stringC = new List<string>();
+            a = "dog";
+            stringC.Add(a);
+            a = "cat";
+            stringC.Add(a);
+            a = "bird";
+            stringC.Add(a);
+            //testing CompareTo()
+            Console.WriteLine($"Results from CompareTo() on (b) is: {stringA.CompareTo(stringC)}");
+
+            //c. Two int lists having different lengths. For example, the first List has three elements, while the second has four
+            Console.WriteLine();
+            Console.WriteLine("Creating (c) Two int lists having different lengths");
+            //list intA
+            List<int> intA = new List<int>();
+            int b = 2;
+            intA.Add(b);
+            b = 4;
+            intA.Add(b);
+            b = 6;
+            intA.Add(b);
+            //list intB
+            List<int> intB = new List<int>();
+            b = 2;
+            intA.Add(b);
+            b = 4;
+            intA.Add(b);
+            b = 6;
+            intA.Add(b);
+            b = 8;
+            intA.Add(b);
+            //testing CompareTo()
+            Console.WriteLine($"Results from CompareTo() on (c) is: {intA.CompareTo(intB)}");
+
+            //d. Two null lists.
+            Console.WriteLine();
+            Console.WriteLine("Creating (d) Two null lists");
+            List<int> intC = new List<int>();
+            List<int> intD = new List<int>();
+            //testing CompareTo()
+            Console.WriteLine($"Results from CompareTo() on (d) is: {intC.CompareTo(intD)}");
+
+            //e. Two identical double lists (i.e., having the same elements following the same order)
+            Console.WriteLine();
+            Console.WriteLine("Creating (e) Two identical double lists");
+            List<double> doubleA = new List<double>();
+            double value = 2.5;
+            doubleA.Add(value);
+            value = 5.5;
+            doubleA.Add(value);
+            value = 8.5;
+            doubleA.Add(value);
+            List<double> doubleB = doubleA;
+            //testing CompareTo()
+            Console.WriteLine($"Results from CompareTo() on (e) is: {doubleA.CompareTo(doubleB)}");
+
+
+
+        }
+    }
+}
